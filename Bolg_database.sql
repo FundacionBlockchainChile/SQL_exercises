2ace59d6440afa41de5d6d54e6753d64b197942e
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-09-15 22:47:58 -0300

    Blog database fixed

--- Bolg_database.sql
+++ Bolg_database.sql
@@ -7,65 +7,190 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema blog_database
+-- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema blog_database
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Blog_Db` DEFAULT CHARACTER SET utf8 ;
-USE `Blog_Db` ;
+CREATE SCHEMA IF NOT EXISTS `blog_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `blog_database`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Blog_Db`.`users` (
+CREATE TABLE IF NOT EXISTS `blog_database`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
-  `address` VARCHAR(255) NULL,
-  `phone` VARCHAR(255) NULL,
-  `isAdmin` BOOLEAN DEFAULT FALSE,
-  `BirthDate` DATE NULL,
-  `created_at` DATETIME NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `phone` VARCHAR(15) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `passwordHash` VARCHAR(50) NOT NULL,
+  `registeredAt` DATE NULL DEFAULT NULL,
+  `lastLogin` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `Blog_Db`.`events`
+-- Table `blog_database`.`posts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Blog_Db`.`Blogs` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  `createdAt` DATETIME NULL,
-  `updatedAt` DATETIME NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `Blog_Db`.`user_eevents`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Blog_Db`.`user_eevents` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NULL,
-  `users_id` INT NOT NULL,
-  `events_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `blog_database`.`posts` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `authotId` BIGINT NOT NULL,
+  `parentId` BIGINT NOT NULL,
+  `title` VARCHAR(75) NOT NULL,
+  `metaTitle` VARCHAR(100) NOT NULL,
+  `slug` VARCHAR(100) NOT NULL,
+  `summary` TINYTEXT NULL DEFAULT NULL,
+  `published` TINYINT(1) NULL DEFAULT NULL,
+  `createdAt` DATE NULL DEFAULT NULL,
+  `updatedAt` DATE NULL DEFAULT NULL,
+  `publishedAt` DATE NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_user_eevents_users_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_user_eevents_events1_idx` (`events_id` ASC) VISIBLE,
-  CONSTRAINT `fk_user_eevents_users`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `Blog_Db`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_eevents_events1`
-    FOREIGN KEY (`events_id`)
-    REFERENCES `Blog_Db`.`events` (`id`)
+  INDEX `fk_posts_user_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_posts_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `blog_database`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- -----------------------------------------------------
+-- Table `blog_database`.`post_category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`post_category` (
+  `postId` BIGINT NOT NULL,
+  `categoryId` BIGINT NOT NULL,
+  `posts_id` BIGINT NOT NULL,
+  PRIMARY KEY (`posts_id`),
+  CONSTRAINT `fk_post_category_posts1`
+    FOREIGN KEY (`posts_id`)
+    REFERENCES `blog_database`.`posts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog_database`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`category` (
+  `id` BIGINT NOT NULL,
+  `parentId` BIGINT NOT NULL,
+  `title` VARCHAR(75) NOT NULL,
+  `metaTitle` VARCHAR(100) NOT NULL,
+  `slug` VARCHAR(100) NOT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  `post_category_posts_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`, `post_category_posts_id`),
+  INDEX `fk_category_post_category1_idx` (`post_category_posts_id` ASC) VISIBLE,
+  CONSTRAINT `fk_category_post_category1`
+    FOREIGN KEY (`post_category_posts_id`)
+    REFERENCES `blog_database`.`post_category` (`posts_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog_database`.`post_comment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`post_comment` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `post_id` BIGINT NOT NULL,
+  `postId` BIGINT NOT NULL,
+  `parentId` BIGINT NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `published` TINYINT(1) NULL DEFAULT NULL,
+  `createdAt` DATE NULL DEFAULT NULL,
+  `publishedAt` DATE NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_post_comment_posts1_idx` (`post_id` ASC) VISIBLE,
+  CONSTRAINT `fk_post_comment_posts1`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `blog_database`.`posts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog_database`.`post_meta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`post_meta` (
+  `id` BIGINT NOT NULL,
+  `postId` BIGINT NOT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  `posts_id` BIGINT NOT NULL,
+  INDEX `fk_post_meta_posts1_idx` (`posts_id` ASC) VISIBLE,
+  CONSTRAINT `fk_post_meta_posts1`
+    FOREIGN KEY (`posts_id`)
+    REFERENCES `blog_database`.`posts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog_database`.`tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`tag` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `title` VARCHAR(75) NOT NULL,
+  `metaTitle` VARCHAR(100) NULL DEFAULT NULL,
+  `slug` VARCHAR(100) NULL DEFAULT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `user_id`),
+  INDEX `fk_tag_user1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_tag_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `blog_database`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog_database`.`post_tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog_database`.`post_tag` (
+  `postId` BIGINT NOT NULL,
+  `tagId` BIGINT NOT NULL,
+  `posts_id` BIGINT NOT NULL,
+  `tag_id` BIGINT NOT NULL,
+  `tag_user_id` INT NOT NULL,
+  PRIMARY KEY (`posts_id`, `tag_id`, `tag_user_id`),
+  INDEX `fk_post_tag_tag1_idx` (`tag_id` ASC, `tag_user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_post_tag_posts1`
+    FOREIGN KEY (`posts_id`)
+    REFERENCES `blog_database`.`posts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_tag_tag1`
+    FOREIGN KEY (`tag_id` , `tag_user_id`)
+    REFERENCES `blog_database`.`tag` (`id` , `user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
